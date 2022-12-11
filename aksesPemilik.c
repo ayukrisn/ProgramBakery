4c78d826a12e1910f734b003f44e6b9150d1d545
ayukrisn <ayuukrisn@gmail.com>
2022-12-11 20:09:59 +0800

    Penambahan header

--- aksesPemilik.c
+++ aksesPemilik.c
@@ -13,6 +13,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+#include <ctype.h>
 #include "logIn.h"
 #include "variabel.h"
 #include "aksesPemilik.h"
--- main.c
+++ main.c
@@ -9,6 +9,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+#include <ctype.h>
 #include "logIn.h"
 #include "variabel.h"
 #include "aksesPemilik.h"
