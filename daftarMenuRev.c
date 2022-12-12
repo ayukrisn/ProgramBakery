88794599e20745e533dd75305d305015d7769e17
ayukrisn <ayuukrisn@gmail.com>
2022-12-13 04:39:32 +0800

    Tambah keterangan

--- daftarMenuRev.c
+++ daftarMenuRev.c
@@ -1,4 +1,10 @@
-
+/*
+==================================================
+    Source code "daftarMenuRev.c" berisi definisi 
+    dari fungsi/prosedur untuk manage menu
+    Contoh: show menu, edit menu, dsb
+==================================================
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,3 +15,27 @@
 #include "variabel.h"
 #include "aksesAdmin.h"
 #include "pendukung.h"
+#include "daftarMenuRev.h"
+
+/*
+    Struct yang digunakan untuk menyimpan
+    data menu dan sat berurusan dengan file
+    write: data dari input pengguna, yang nanti akan digunakan
+           untuk dimasukkan ke file/variabel program
+    read:  data yang dibaca dari file, yang nanti dimasukkan
+           ke variabel di dalam program
+    data:  variabel yang digunakan untuk menyimpan data
+           selama program dijalankan
+*/
+dataMenu data; dataMenu write; dataMenu read;
+
+/*
+    Melihat daftar menu
+*/
+// void lihatDaftarMenu()
+
+
+/*
+    Menambah daftar menu (hanya dapat dilakukan pemilik)
+*/
+
