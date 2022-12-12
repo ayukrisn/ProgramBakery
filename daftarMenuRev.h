e29e7fcce802214fd8ef56f841565d8ba704c0c1
ayukrisn <ayuukrisn@gmail.com>
2022-12-13 04:39:44 +0800

    Tambah Keterangan

---
+++ daftarMenuRev.h
@@ -0,0 +1,20 @@
+/*
+==================================================
+    Header file "daftarMenuRev.h" berisi deklarasi 
+    dari prosedur, fungsi, atau pun variabel yang 
+    didefinisikan di dalam daftarMenuRev.c
+==================================================
+*/
+
+/*
+    IFNDEF, DEFINE, DAN ENDIF
+    Header guard yang digunakan untuk mencegah
+    double inclusion saat menangani include directive
+*/
+#ifndef DAFTAR_MENU_H
+#define DAFTAR_MENU_H
+
+
+
+
+#endif //DAFTAR_MENU_H
